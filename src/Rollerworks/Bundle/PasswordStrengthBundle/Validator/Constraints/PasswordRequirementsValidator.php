6464ae9dbd4e95bd53d348fc43ff483878e14035
Sebastiaan Stok <s.stok@rollerscapes.net>
2014-08-27 17:16:55 +0200

    add file header

--- src/Rollerworks/Bundle/PasswordStrengthBundle/Validator/Constraints/PasswordRequirementsValidator.php
+++ src/Rollerworks/Bundle/PasswordStrengthBundle/Validator/Constraints/PasswordRequirementsValidator.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of the RollerworksPasswordStrengthBundle package.
+ *
+ * (c) 2012-2014 Sebastiaan Stok <s.stok@rollerscapes.net>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Rollerworks\Bundle\PasswordStrengthBundle\Validator\Constraints;
 
 use Symfony\Component\Validator\Constraint;
