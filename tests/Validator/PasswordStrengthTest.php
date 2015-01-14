7ddb52f6b453de7a51e9e19c31569ab38847ac67
Sebastiaan Stok <s.stok@rollerscapes.net>
2015-01-14 11:12:21 +0100

    remove copyright year from header
    Use only license.md for the copyright year ranges
    
    [skip ci]

--- tests/Validator/PasswordStrengthTest.php
+++ tests/Validator/PasswordStrengthTest.php
@@ -1,9 +1,9 @@
 <?php
 
-/**
+/*
  * This file is part of the RollerworksPasswordStrengthBundle package.
  *
- * (c) 2012-2014 Sebastiaan Stok <s.stok@rollerscapes.net>
+ * (c) Sebastiaan Stok <s.stok@rollerscapes.net>
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
