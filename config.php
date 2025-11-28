c1212b02fa6c6b66a98ffdd3f72ca7a402fb11dd
Sebastiaan Stok <s.stok@rollerscapes.net>
2025-11-28 21:54:03 +0100

    Add configuration

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+// See https://hupkit.github.io/hupkit/config.html
+
+return [
+    'main_branch' => 'main',
+    'branches' => [],
+    'host' => 'github.com',
+    'repository' => 'rollerworks/PasswordStrengthValidator',
+    'schema_version' => 2,
+];
